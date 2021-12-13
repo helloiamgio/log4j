463a16931fb2dc88411d36c3ade3529c30055db0
helloiamgio <helloiamgio@gmail.com>
2021-12-13 12:26:21 +0100

    Update find-log4j.sh

--- find-log4j.sh
+++ find-log4j.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ `id -u` -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 TIMESPATMP=$(date "+%Y.%m.%d-%H.%M.%S")
-echo "Running CVE-2021-44228 mitigation on $(pwd)"
-echo "Log4j 2 Zero-day mitigation script (CVE-2021-44228)" | tee -a "log4j_mitigation_$TIMESPATMP.log"
+echo "Running CVE-2021-44228"
+echo "Log4j 2 Zero-day find script (CVE-2021-44228)" | tee -a "log4j_mitigation_$TIMESPATMP.log"
 JAR_FILES=$(find / -name '*.jar')
 for JAR_FILE in $JAR_FILES
 do
